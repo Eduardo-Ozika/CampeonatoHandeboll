356f031875b21ad5f73bf3db49b54f9df482f6f8
Eduardo-Ozika <eduardoozika.ti@gmail.com>
2022-11-07 01:37:12 -0300

    classe Stadium

---
+++ src/main/java/com/projeto/projetoHandeball/model/entity/Stadium.java
@@ -0,0 +1,27 @@
+package main.java.com.projeto.projetoHandeball.model.entity;
+
+public class Stadium {
+    private String name;
+    private Address address;
+
+    public Stadium(String name, Address address) {
+        this.name = name;
+        this.address = address;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
+    }
+}
